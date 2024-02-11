969e6fa12bbaf5a1f7ec8575b5b20c779971a6f9
Maksim Ryzhikov <rv.maksim@gmail.com>
2024-02-11 22:23:01 +0300

    chore: services

---
+++ services/foo-2/src/index.ts
@@ -0,0 +1,9 @@
+import * as http from 'http';
+const port = process.env.APP_PORT ?? 3000;
+const server = http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ name: 'foo-2' }));
+});
+server.listen(port, () => {
+  console.log('server is listening on ' + port);
+});
