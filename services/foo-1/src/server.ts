7c338955e837676e676bbb4b5b50c2751e35efce
Maksim Ryzhikov <rv.maksim@gmail.com>
2024-02-24 22:41:00 +0300

    chore: cleanup

--- services/foo-1/src/server.ts
+++ services/foo-1/src/server.ts
@@ -4,26 +4,24 @@ import { foo1 } from '@packages/foo-1/src/index';
 foo1();
 
 const config = {
-  foo2Url: process.env.FOO_2_URL || 'http://localhost:3002',
-  foo3Url: process.env.FOO_3_URL || 'http://localhost:3003',
+  foo2: { url: process.env.FOO_2_URL || 'http://localhost:3002' },
+  foo3: { url: process.env.FOO_3_URL || 'http://localhost:3003' }
 }
 
-Object.keys(config).forEach(key => {
-  console.log(`config: ${key}=${config[key]}`);
-});
+console.log('config', config);
 
 export const server = http.createServer(async function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
 
   if (req.url === '/proxy/foo-2') {
-    const response = await fetch(config.foo2Url).then(response => response.json());
+    const response = await fetch(config.foo2.url).then(response => response.json());
     console.log('response', response);
     res.end(JSON.stringify(response));
     return;
   }
 
   if (req.url === '/proxy/foo-3') {
-    const response = await fetch(config.foo3Url).then(response => response.json());
+    const response = await fetch(config.foo3.url).then(response => response.json());
     console.log('response', response);
     res.end(JSON.stringify(response));
     return;
